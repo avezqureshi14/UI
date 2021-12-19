742db9672778433071ca820523ca4d3906bea7d6
Avez <avezqureshi14@gmail.com>
2021-12-19 13:35:20 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const resi0 = document.querySelector('.resi0');
+const resi1 = document.querySelector('.resi1');
+const resi2 = document.querySelector('.resi2');
+const resi3 = document.querySelector('.resi3');
+const openMenu = document.querySelector('.openMenu');
+
+ 
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+resi0.addEventListener('click',close);
+resi1.addEventListener('click',close);
+resi2.addEventListener('click',close);
+resi3.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '-10px';
+}
+function close(){
+    mainMenu.style.top = '-103%';
+    
+}
